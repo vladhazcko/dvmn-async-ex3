cf67cb753344f1ad9aea3ebf2b81a93984431f1e
vlad <vladxazcko>
2020-04-28 21:34:49 +0300

    upd

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+from asyncio import subprocess
+import time
+
+
+def main():
+    asyncio.run(archivate())
+
+
+async def archivate():
+    command = 'zip -r - test'
+
+    process = await subprocess.create_subprocess_shell(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    with open('test.zip', 'wb') as zip_file:
+        while True:
+
+            archive_chunk = await process.stdout.read(100)
+            zip_file.write(archive_chunk)
+            if not archive_chunk:
+                break
+
+
+if __name__ == '__main__':
+    main()
